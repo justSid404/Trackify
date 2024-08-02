fde02b5b86df5eaaf9328eebfe879e91da96dba6
Siddhant Pawar <79016942+justSid404@users.noreply.github.com>
2024-08-03 03:13:59 +0530

    Added Code to show command prompt to take input from user

--- scripts/home.js
+++ scripts/home.js
@@ -22,26 +22,74 @@ if(trackers.length === 0) {
 //Code to add new Tracker
 createTrackerElement.addEventListener('click', () => {
 
-  const newCardhtml = `
+  takeInputThroughPrompt();
 
-  <div class="tracker-card tracker-card-${trackers.length}">
-    <div class="tracker">
-      +
+});
+
+function takeInputThroughPrompt() {
+
+  const promptHtml = `
+
+  <div class="input-prompt">
+
+    <div class="input-prompt-box">
+      What will the Tracker name?
+      <label class="input-prompt-label">
+
+        <input class="input-prompt-checkbox" type="checkbox">Recurring Tasks?
+
+      </label>
+      <input class="input-prompt-textbox" type="text">
+      <div class="input-prompt-buttons">
+        <button class="input-prompt-save">Save</button>
+        <button class="input-prompt-cancel">Cancel</button>
+      </div>
+      
     </div>
+
   </div>`;
 
-  cardHolderElement.insertAdjacentHTML('afterbegin', newCardhtml);
-  cardHolderElement.classList.remove('card-holder-zero');
-  document.querySelector(`.card-holder`).scrollTo({
-    left: 0,
-    behavior: 'smooth'
+  document.body.insertAdjacentHTML('afterbegin', promptHtml);
+
+  document.querySelector('.input-prompt-save').addEventListener('click', () => {
+
+    const inputValue = document.querySelector('.input-prompt-textbox').value;
+    document.querySelector('.input-prompt').remove();
+    addTrackerCard(inputValue);
+  
+  });
+  
+  document.querySelector('.input-prompt-cancel').addEventListener('click', () => {
+  
+    document.querySelector('.input-prompt').remove();
+  
   });
 
-  trackers.push({
-    id: trackers.length,
-    name: `tracker-card-${trackers.length}`
-  });
+}
 
-  console.log(trackers);
+function addTrackerCard(inputValue) {
 
-})
+  const newCardhtml = `
+
+    <div class="tracker-card tracker-card-${trackers.length}">
+      <div class="tracker">
+        ${inputValue}
+      </div>
+    </div>`;
+
+    cardHolderElement.insertAdjacentHTML('afterbegin', newCardhtml);
+    cardHolderElement.classList.remove('card-holder-zero');
+    document.querySelector(`.card-holder`).scrollTo({
+      left: 0,
+      behavior: 'smooth'
+    });
+
+    trackers.push({
+      id: trackers.length,
+      elementClass: `tracker-card-${trackers.length}`,
+      name: inputValue
+    });
+
+    console.log(trackers);
+
+}
