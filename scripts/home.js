6f1e2b0b70223088b4c95edc33113fde306a89cd
Siddhant Pawar <79016942+justSid404@users.noreply.github.com>
2024-08-03 00:34:01 +0530

    Added code to add new tracker card in home page

--- scripts/home.js
+++ scripts/home.js
@@ -1,4 +1,8 @@
 let userLogged;
+let trackers = [];
+
+const cardHolderElement = document.querySelector('.card-holder');
+const createTrackerElement = document.querySelector('.create-tracker-card');
 
 if(localStorage.getItem('userLogged') !== null) {
   userLogged = JSON.parse(localStorage.getItem('userLogged'));
@@ -7,3 +11,26 @@ if(localStorage.getItem('userLogged') !== null) {
 
 //Home page default transition
 document.body.classList.add('fade-in');
+
+createTrackerElement.addEventListener('click', () => {
+
+  const newCardhtml = `
+
+  <div class="tracker-card tracker-card-${trackers.length}">
+    <div class="tracker">
+      +
+    </div>
+  </div>`;
+
+  cardHolderElement.insertAdjacentHTML('afterbegin', newCardhtml);
+  document.querySelector(`.card-holder`).scrollTo({
+    left: 0,
+    behavior: 'smooth'
+  });
+
+  trackers.push({
+    id: trackers.length,
+    name: `tracker-card-${trackers.length}`
+  });
+
+})
