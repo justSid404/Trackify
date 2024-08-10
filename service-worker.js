874af032daf5bcce2042dcc3687b337b6fe2fcc4
Siddhant Pawar <79016942+justSid404@users.noreply.github.com>
2024-08-11 00:47:59 +0530

    Another attempt to add PWA support

---
+++ service-worker.js
@@ -0,0 +1,12 @@
+self.addEventListener('install', (event) => {
+  // Perform install steps (if needed)
+  console.log('Service Worker installed');
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
