31f424d676add3ed96df96c468af08b787913a9f
Siddhant Pawar <79016942+justSid404@users.noreply.github.com>
2024-08-11 02:38:31 +0530

    updated service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,30 @@
 self.addEventListener('install', (event) => {
-  // Perform install steps (if needed)
-  console.log('Service Worker installed');
+  event.waitUntil(
+    caches.open('my-app-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/service-worker.js',
+        '/manifest.json',
+        '/index.html',
+        '/login.html',
+        '/home.html',
+        '/error.html',
+        '/styles/Theme_Light.css',
+        '/styles/Theme_Dark.css',
+        '/styles/Theme_Dark-Home.css',
+        '/scripts/default-page-transition.js',
+        '/scripts/home.js',
+        '/scripts/login.js',
+        '/images/icon.png'
+      ]);
+    })
+  );
 });
 
-// self.addEventListener('fetch', (event) => {
-//   event.respondWith(
-//     caches.match(event.request).then((response) => {
-//       return response || fetch(event.request);
-//     })
-//   );
-// });
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
