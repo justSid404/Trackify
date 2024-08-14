1b96aef4ea710729a04be0a06e06170a635731a4
Siddhant Pawar <79016942+justSid404@users.noreply.github.com>
2024-08-14 06:42:52 +0530

    Added basic XP and Level system

--- scripts/home.js
+++ scripts/home.js
@@ -14,6 +14,8 @@ const rightArrowBtnElement = document.querySelector('.traverse-right-button');
 const leftArrowBtnElement = document.querySelector('.traverse-left-button');
 
 let userAtCard = 0;
+let userXP = 0;
+let userLevel = 1;
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -349,6 +351,12 @@ async function addEventToTaskAction(taskActionElement) {
 
     } else if(taskActionElement.value === "inpro") {
 
+      if(trackers[tempTrackerNo].task[tempTaskNo].status === "done") {
+
+        xpAddOrSubtract("subtract", 5);
+
+      }
+
       trackers[tempTrackerNo].task[tempTaskNo].status = "inpro";
       tempTaskElement.classList.remove('task-todo');
       tempTaskElement.classList.add('task-inpro');
@@ -356,10 +364,18 @@ async function addEventToTaskAction(taskActionElement) {
 
     } else if(taskActionElement.value === "done") {
 
+      if(trackers[tempTrackerNo].task[tempTaskNo].status !== "done") {
+
+        xpAddOrSubtract("add", 5);
+
+      }
+
       trackers[tempTrackerNo].task[tempTaskNo].status = "done";
       tempTaskElement.classList.remove('task-todo');
       tempTaskElement.classList.remove('task-inpro');
       tempTaskElement.classList.add('task-done');
+
+      confettiAnimation();
 
     } else if(taskActionElement.value === "edit") {
 
@@ -554,6 +570,10 @@ async function getUserData() {
           document.querySelector(`.content-tracker-card-${trackerLength}`).insertAdjacentHTML('beforeend', taskHtml);
       
           document.querySelector(`.task-${taskIndex}-action-tracker-card-${trackerLength}`).value = taskItem.status;
+
+          if(taskItem.status === "done") {
+            userXP += 5;
+          }
             
           const tempTaskActionElement = document.querySelector(`.task-${taskIndex}-action-tracker-card-${trackerLength}`);
           addEventToTaskAction(tempTaskActionElement);
@@ -595,8 +615,14 @@ async function getUserData() {
   
   });
 
+  //Calculate level and XP
+  calculateLevelAndXP();
+
   //Home page default transition
   document.body.classList.add('fade-in');
+
+  console.log('User XP: '+userXP);
+  console.log('User Level: '+userLevel);
 
 }
 
@@ -1172,5 +1198,738 @@ async function initializeApp_phase2() {
     }
   
   });
+  
+}
+
+//Code to handle Level increament
+async function levelHandler() {
+
+  if(userLevel >= 1 && userLevel <= 5) {
+
+    if(userXP >= 100) {
+
+      userLevel++;
+      const diff = userXP - 100;
+      userXP = diff;
+
+      starExplodeAnimation();
+
+    }
+
+  } else if(userLevel >= 6 && userLevel <= 10) {
+
+    if(userXP >= 200) {
+
+      userLevel++;
+      const diff = userXP - 200;
+      userXP = diff;
+
+      starExplodeAnimation();
+
+    }
+    
+  } else if(userLevel >= 10 && userLevel <= 15) {
+
+    if(userXP >= 300) {
+
+      userLevel++;
+      const diff = userXP - 300;
+      userXP = diff;
+
+      starExplodeAnimation();
+
+    }
+    
+  } else if(userLevel >= 16 && userLevel <= 20) {
+
+    if(userXP >= 400) {
+
+      userLevel++;
+      const diff = userXP - 400;
+      userXP = diff;
+
+      starExplodeAnimation();
+
+    }
+    
+  } else if(userLevel >= 21 && userLevel <= 25) {
+
+    if(userXP >= 500) {
+
+      userLevel++;
+      const diff = userXP - 500;
+      userXP = diff;
+
+      starExplodeAnimation();
+
+    }
+    
+  } else if(userLevel >= 26 && userLevel <= 30) {
+
+    if(userXP >= 600) {
+
+      userLevel++;
+      const diff = userXP - 600;
+      userXP = diff;
+
+      starExplodeAnimation();
+
+    }
+    
+  } else if(userLevel >= 31 && userLevel <= 35) {
+
+    if(userXP >= 700) {
+
+      userLevel++;
+      const diff = userXP - 700;
+      userXP = diff;
+
+      starExplodeAnimation();
+
+    }
+    
+  } else if(userLevel >= 36 && userLevel <= 40) {
+
+    if(userXP >= 800) {
+
+      userLevel++;
+      const diff = userXP - 800;
+      userXP = diff;
+
+      starExplodeAnimation();
+
+    }
+    
+  } else if(userLevel >= 41 && userLevel <= 45) {
+
+    if(userXP >= 900) {
+
+      userLevel++;
+      const diff = userXP - 900;
+      userXP = diff;
+
+      starExplodeAnimation();
+
+    }
+    
+  } else if(userLevel >= 46 && userLevel <= 50) {
+
+    if(userXP >= 1000) {
+
+      userLevel++;
+      const diff = userXP - 1000;
+      userXP = diff;
+
+      starExplodeAnimation();
+
+    }
+    
+  } else if(userLevel >= 51 && userLevel <= 55) {
+
+    if(userXP >= 1100) {
+
+      userLevel++;
+      const diff = userXP - 1100;
+      userXP = diff;
+
+      starExplodeAnimation();
+
+    }
+    
+  } else if(userLevel >= 56 && userLevel <= 60) {
+
+    if(userXP >= 1200) {
+
+      userLevel++;
+      const diff = userXP - 1200;
+      userXP = diff;
+
+      starExplodeAnimation();
+
+    }
+    
+  } else if(userLevel >= 61 && userLevel <= 65) {
+
+    if(userXP >= 1300) {
+
+      userLevel++;
+      const diff = userXP - 1300;
+      userXP = diff;
+
+      starExplodeAnimation();
+
+    }
+    
+  } else if(userLevel >= 66 && userLevel <= 70) {
+
+    if(userXP >= 1400) {
+
+      userLevel++;
+      const diff = userXP - 1400;
+      userXP = diff;
+
+      starExplodeAnimation();
+
+    }
+    
+  } else if(userLevel >= 71 && userLevel <= 75) {
+
+    if(userXP >= 1500) {
+
+      userLevel++;
+      const diff = userXP - 1500;
+      userXP = diff;
+
+      starExplodeAnimation();
+
+    }
+    
+  } else if(userLevel >= 76 && userLevel <= 80) {
+
+    if(userXP >= 1600) {
+
+      userLevel++;
+      const diff = userXP - 1600;
+      userXP = diff;
+
+      starExplodeAnimation();
+
+    }
+    
+  } else if(userLevel >= 81 && userLevel <= 85) {
+
+    if(userXP >= 1700) {
+
+      userLevel++;
+      const diff = userXP - 1700;
+      userXP = diff;
+
+      starExplodeAnimation();
+
+    }
+    
+  } else if(userLevel >= 86 && userLevel <= 90) {
+
+    if(userXP >= 1800) {
+
+      userLevel++;
+      const diff = userXP - 1800;
+      userXP = diff;
+
+      starExplodeAnimation();
+
+    }
+    
+  } else if(userLevel >= 91 && userLevel <= 95) {
+
+    if(userXP >= 1900) {
+
+      userLevel++;
+      const diff = userXP - 1900;
+      userXP = diff;
+
+      starExplodeAnimation();
+
+    }
+    
+  } else if(userLevel >= 96 && userLevel <= 100) {
+
+    if(userXP >= 2000) {
+
+      userLevel++;
+      const diff = userXP - 2000;
+      userXP = diff;
+
+      starExplodeAnimation();
+
+    }
+    
+  } else if(userLevel > 100) {
+
+    if(userXP >= 2500) {
+
+      userLevel++;
+      const diff = userXP - 2500;
+      userXP = diff;
+
+      starExplodeAnimation();
+
+    }
+    
+  }
+  
+}
+
+//Code to add or deduct XP according to task action
+async function xpAddOrSubtract(operation, value) {
+  
+  if(operation === "+" || operation === "add") {
+
+    userXP += value;
+
+  } else if(operation === "-" || operation === "subtract" || operation === "minus") {
+
+    userXP -= value;
+
+  }
+
+  if(userXP > 0) {
+
+    levelHandler();
+
+  } else if(userXP < 0) {
+
+    userLevel--;
+    
+    if(userLevel >= 1 && userLevel <= 5) {
+
+      userXP = 100 + userXP;
+  
+    } else if(userLevel >= 6 && userLevel <= 10) {
+
+      userXP = 200 + userXP;
+      
+    } else if(userLevel >= 10 && userLevel <= 15) {
+
+      userXP = 300 + userXP;
+      
+    } else if(userLevel >= 16 && userLevel <= 20) {
+
+      userXP = 400 + userXP;
+      
+    } else if(userLevel >= 21 && userLevel <= 25) {
+
+      userXP = 500 + userXP;
+      
+    } else if(userLevel >= 26 && userLevel <= 30) {
+
+      userXP = 600 + userXP;
+      
+    } else if(userLevel >= 31 && userLevel <= 35) {
+
+      userXP = 700 + userXP;
+      
+    } else if(userLevel >= 36 && userLevel <= 40) {
+
+      userXP = 800 + userXP;
+      
+    } else if(userLevel >= 41 && userLevel <= 45) {
+
+      userXP = 900 + userXP;
+      
+    } else if(userLevel >= 46 && userLevel <= 50) {
+
+      userXP = 1000 + userXP;
+      
+    } else if(userLevel >= 51 && userLevel <= 55) {
+
+      userXP = 1100 + userXP;
+      
+    } else if(userLevel >= 56 && userLevel <= 60) {
+
+      userXP = 1200 + userXP;
+      
+    } else if(userLevel >= 61 && userLevel <= 65) {
+
+      userXP = 1300 + userXP;
+      
+    } else if(userLevel >= 66 && userLevel <= 70) {
+
+      userXP = 1400 + userXP;
+      
+    } else if(userLevel >= 71 && userLevel <= 75) {
+
+      userXP = 1500 + userXP;
+      
+    } else if(userLevel >= 76 && userLevel <= 80) {
+
+      userXP = 1600 + userXP;
+      
+    } else if(userLevel >= 81 && userLevel <= 85) {
+
+      userXP = 1700 + userXP;
+      
+    } else if(userLevel >= 86 && userLevel <= 90) {
+
+      userXP = 1800 + userXP;
+      
+    } else if(userLevel >= 91 && userLevel <= 95) {
+
+      userXP = 1900 + userXP;
+      
+    } else if(userLevel >= 96 && userLevel <= 100) {
+
+      userXP = 2000 + userXP;
+      
+    } else if(userLevel > 100) {
+
+      userXP = 2500 + userXP;
+      
+    }
+
+  }
+
+  console.log('User XP: '+userXP);
+  console.log('User Level: '+userLevel);
+
+}
+
+async function confettiAnimation() {
+
+  // do this for 1 seconds
+  var duration = 1 * 1000;
+  var end = Date.now() + duration;
+
+  (function frame() {
+    // launch a few confetti from the bottom center
+    confetti({
+      particleCount: 7,
+      angle: 90,
+      spread: 80,
+      origin: { x: Math.random(), y: Math.random() }
+    });
+
+    // keep going until we are out of time
+    if (Date.now() < end) {
+      requestAnimationFrame(frame);
+    }
+  }());
+  
+}
+
+async function starExplodeAnimation() {
+
+  // do this for 1 seconds
+  var duration = 1 * 1000;
+  var end = Date.now() + duration;
+
+  (function frame() {
+    // launch a few confetti from the bottom center
+    confetti({
+      spread: 360,
+      gravity: 0,
+      decay: 0.94,
+      startVelocity: 20,
+      particleCount: 3,
+      shapes: ['star'],
+      colors: ['FFE400', 'FFBD00', 'E89400', 'FFAC6C', 'FDFFB8']
+    });
+
+    // keep going until we are out of time
+    if (Date.now() < end) {
+      requestAnimationFrame(frame);
+    }
+  }());
+  
+}
+
+async function calculateLevelAndXP() {
+
+  if(userXP < 100) {
+    userLevel = 1;
+  } else if(userXP >= 100 && userXP <= 199) {
+    userLevel = 2;
+    userXP -= 100;
+  } else if(userXP >= 200 && userXP <= 299) {
+    userLevel = 3;
+    userXP -= 200;
+  } else if(userXP >= 300 && userXP <= 399) {
+    userLevel = 4;
+    userXP -= 300;
+  } else if(userXP >= 400 && userXP <= 599) {
+    userLevel = 5;
+    userXP -= 400;
+  } else if(userXP >= 600 && userXP <= 799) {
+    userLevel = 6;
+    userXP -= 600;
+  } else if(userXP >= 800 && userXP <= 999) {
+    userLevel = 7;
+    userXP -= 800;
+  } else if(userXP >= 1000 && userXP <= 1199) {
+    userLevel = 8;
+    userXP -= 1000;
+  } else if(userXP >= 1200 && userXP <= 1399) {
+    userLevel = 9;
+    userXP -= 1200;
+  } else if(userXP >= 1400 && userXP <= 1699) {
+    userLevel = 10;
+    userXP -= 1400;
+  } else if(userXP >= 1700 && userXP <= 1999) {
+    userLevel = 11;
+    userXP -= 1700;
+  } else if(userXP >= 2000 && userXP <= 2299) {
+    userLevel = 12;
+    userXP -= 2000;
+  } else if(userXP >= 2300 && userXP <= 2599) {
+    userLevel = 13;
+    userXP -= 2300;
+  } else if(userXP >= 2600 && userXP <= 2899) {
+    userLevel = 14;
+    userXP -= 2600;
+  } else if(userXP >= 2900 && userXP <= 3299) {
+    userLevel = 15;
+    userXP -= 2900;
+  } else if(userXP >= 3300 && userXP <= 3699) {
+    userLevel = 16;
+    userXP -= 3300;
+  } else if(userXP >= 3700  && userXP <= 4099) {
+    userLevel = 17;
+    userXP -= 3700;
+  } else if(userXP >= 4100  && userXP <= 4499) {
+    userLevel = 18;
+    userXP -= 4100;
+  } else if(userXP >= 4500  && userXP <= 4899) {
+    userLevel = 19;
+    userXP -= 4500;
+  } else if(userXP >= 4900  && userXP <= 5399) {
+    userLevel = 20;
+    userXP -= 4900;
+  } else if(userXP >= 5400  && userXP <= 5899) {
+    userLevel = 21;
+    userXP -= 5400;
+  } else if(userXP >= 5900  && userXP <= 6399) {
+    userLevel = 22;
+    userXP -= 5900;
+  } else if(userXP >= 6400  && userXP <= 6899) {
+    userLevel = 23;
+    userXP -= 6400;
+  } else if(userXP >= 6900  && userXP <= 7399) {
+    userLevel = 24;
+    userXP -= 6900;
+  } else if(userXP >= 7400  && userXP <= 7999) {
+    userLevel = 25;
+    userXP -= 7400;
+  } else if(userXP >= 8000  && userXP <= 8599) {
+    userLevel = 26;
+    userXP -= 8000;
+  } else if(userXP >= 8600  && userXP <= 9199) {
+    userLevel = 27;
+    userXP -= 8600;
+  } else if(userXP >= 9200  && userXP <= 9799) {
+    userLevel = 28;
+    userXP -= 9200;
+  } else if(userXP >= 9800  && userXP <= 10399) {
+    userLevel = 29;
+    userXP -= 9800;
+  } else if(userXP >= 10400  && userXP <= 11099) {
+    userLevel = 30;
+    userXP -= 10400;
+  } else if(userXP >= 11100  && userXP <= 11799) {
+    userLevel = 31;
+    userXP -= 11100;
+  } else if(userXP >= 11800  && userXP <= 12499) {
+    userLevel = 32;
+    userXP -= 11800;
+  } else if(userXP >= 12500  && userXP <= 13199) {
+    userLevel = 33;
+    userXP -= 12500;
+  } else if(userXP >= 13200  && userXP <= 1399) {
+    userLevel = 34;
+    userXP -= 13200;
+  } else if(userXP >= 13900  && userXP <= 14699) {
+    userLevel = 35;
+    userXP -= 13900;
+  } else if(userXP >= 14700  && userXP <= 15499) {
+    userLevel = 36;
+    userXP -= 14700;
+  } else if(userXP >= 15500  && userXP <= 16299) {
+    userLevel = 37;
+    userXP -= 15500;
+  } else if(userXP >= 16300  && userXP <= 17099) {
+    userLevel = 38;
+    userXP -= 16300;
+  } else if(userXP >= 17100  && userXP <= 17899) {
+    userLevel = 39;
+    userXP -= 17100;
+  } else if(userXP >= 17900  && userXP <= 18799) {
+    userLevel = 40;
+    userXP -= 17900;
+  } else if(userXP >= 18800  && userXP <= 19699) {
+    userLevel = 41;
+    userXP -= 18800;
+  } else if(userXP >= 19700  && userXP <= 20599) {
+    userLevel = 42;
+    userXP -= 19700;
+  } else if(userXP >= 20600  && userXP <= 21499) {
+    userLevel = 43;
+    userXP -= 20600;
+  } else if(userXP >= 21500  && userXP <= 22399) {
+    userLevel = 44;
+    userXP -= 21500;
+  } else if(userXP >= 22400  && userXP <= 23399) {
+    userLevel = 45;
+    userXP -= 22400;
+  } else if(userXP >= 23400  && userXP <= 24399) {
+    userLevel = 46;
+    userXP -= 23400;
+  } else if(userXP >= 24400  && userXP <= 25399) {
+    userLevel = 47;
+    userXP -= 24400;
+  } else if(userXP >= 25400  && userXP <= 26399) {
+    userLevel = 48;
+    userXP -= 25400;
+  } else if(userXP >= 26400  && userXP <= 27399) {
+    userLevel = 49;
+    userXP -= 26400;
+  } else if(userXP >= 27400  && userXP <= 28499) {
+    userLevel = 50;
+    userXP -= 27400;
+  } else if(userXP >= 28500  && userXP <= 29599) {
+    userLevel = 51;
+    userXP -= 28500;
+  } else if(userXP >= 29600  && userXP <= 30699) {
+    userLevel = 52;
+    userXP -= 29600;
+  } else if(userXP >= 30700  && userXP <= 31799) {
+    userLevel = 53;
+    userXP -= 30700;
+  } else if(userXP >= 31800  && userXP <= 32899) {
+    userLevel = 54;
+    userXP -= 31800;
+  } else if(userXP >= 32900  && userXP <= 33999) {
+    userLevel = 55;
+    userXP -= 32900;
+  } else if(userXP >= 34000  && userXP <= 35199) {
+    userLevel = 56;
+    userXP -= 34000;
+  } else if(userXP >= 35200  && userXP <= 36399) {
+    userLevel = 57;
+    userXP -= 35200;
+  } else if(userXP >= 36400  && userXP <= 37599) {
+    userLevel = 58;
+    userXP -= 36400;
+  } else if(userXP >= 37600  && userXP <= 38799) {
+    userLevel = 59;
+    userXP -= 37600;
+  } else if(userXP >= 38800  && userXP <= 40099) {
+    userLevel = 60;
+    userXP -= 38800;
+  } else if(userXP >= 40100  && userXP <= 41399) {
+    userLevel = 61;
+    userXP -= 40100;
+  } else if(userXP >= 41400  && userXP <= 42699) {
+    userLevel = 62;
+    userXP -= 41400;
+  } else if(userXP >= 42700  && userXP <= 43999) {
+    userLevel = 63;
+    userXP -= 42700;
+  } else if(userXP >= 44000  && userXP <= 45299) {
+    userLevel = 64;
+    userXP -= 44000;
+  } else if(userXP >= 45300  && userXP <= 46699) {
+    userLevel = 65;
+    userXP -= 45300;
+  } else if(userXP >= 46700  && userXP <= 48099) {
+    userLevel = 66;
+    userXP -= 46700;
+  } else if(userXP >= 48100  && userXP <= 49499) {
+    userLevel = 67;
+    userXP -= 48100;
+  } else if(userXP >= 49500  && userXP <= 50899) {
+    userLevel = 68;
+    userXP -= 49500;
+  } else if(userXP >= 50900  && userXP <= 52299) {
+    userLevel = 69;
+    userXP -= 50900;
+  } else if(userXP >= 52300  && userXP <= 53799) {
+    userLevel = 70;
+    userXP -= 52300;
+  } else if(userXP >= 53800  && userXP <= 55299) {
+    userLevel = 71;
+    userXP -= 53800;
+  } else if(userXP >= 55300  && userXP <= 56799) {
+    userLevel = 72;
+    userXP -= 55300;
+  } else if(userXP >= 56800  && userXP <= 58299) {
+    userLevel = 73;
+    userXP -= 56800;
+  } else if(userXP >= 58300  && userXP <= 59799) {
+    userLevel = 74;
+    userXP -= 58300;
+  } else if(userXP >= 59800  && userXP <= 61399) {
+    userLevel = 75;
+    userXP -= 59800;
+  } else if(userXP >= 61400  && userXP <= 62999) {
+    userLevel = 76;
+    userXP -= 61400;
+  } else if(userXP >= 63000  && userXP <= 64599) {
+    userLevel = 77;
+    userXP -= 63000;
+  } else if(userXP >= 64600  && userXP <= 66199) {
+    userLevel = 78;
+    userXP -= 64600;
+  } else if(userXP >= 66200  && userXP <= 67799) {
+    userLevel = 79;
+    userXP -= 66200;
+  } else if(userXP >= 67800  && userXP <= 69499) {
+    userLevel = 80;
+    userXP -= 67800;
+  } else if(userXP >= 69500  && userXP <= 71199) {
+    userLevel = 81;
+    userXP -= 69500;
+  } else if(userXP >= 71200  && userXP <= 72899) {
+    userLevel = 82;
+    userXP -= 71200;
+  } else if(userXP >= 72900  && userXP <= 74599) {
+    userLevel = 83;
+    userXP -= 72900;
+  } else if(userXP >= 74600  && userXP <= 76299) {
+    userLevel = 84;
+    userXP -= 74600;
+  } else if(userXP >= 76300  && userXP <= 78099) {
+    userLevel = 85;
+    userXP -= 76300;
+  } else if(userXP >= 78100  && userXP <= 79899) {
+    userLevel = 86;
+    userXP -= 78100;
+  } else if(userXP >= 79900  && userXP <= 81699) {
+    userLevel = 87;
+    userXP -= 79900;
+  } else if(userXP >= 81700  && userXP <= 83499) {
+    userLevel = 88;
+    userXP -= 81700;
+  } else if(userXP >= 83500  && userXP <= 85299) {
+    userLevel = 89;
+    userXP -= 83500;
+  } else if(userXP >= 85300  && userXP <= 87199) {
+    userLevel = 90;
+    userXP -= 85300;
+  } else if(userXP >= 87200  && userXP <= 89099) {
+    userLevel = 91;
+    userXP -= 87200;
+  } else if(userXP >= 89100  && userXP <= 90999) {
+    userLevel = 92;
+    userXP -= 89100;
+  } else if(userXP >= 91000  && userXP <= 92899) {
+    userLevel = 93;
+    userXP -= 91000;
+  } else if(userXP >= 92900  && userXP <= 94799) {
+    userLevel = 94;
+    userXP -= 92900;
+  } else if(userXP >= 94800  && userXP <= 96999) {
+    userLevel = 95;
+    userXP -= 94800;
+  } else if(userXP >= 97000  && userXP <= 98999) {
+    userLevel = 96;
+    userXP -= 97000;
+  } else if(userXP >= 99000  && userXP <= 100999) {
+    userLevel = 97;
+    userXP -= 99000;
+  } else if(userXP >= 101000 && userXP <= 102999) {
+    userLevel = 98;
+    userXP -= 101000;
+  } else if(userXP >= 103000 && userXP <= 104999) {
+    userLevel = 99;
+    userXP -= 103000;
+  } else if(userXP >= 105000 && userXP <= 106999) {
+    userLevel = 100;
+    userXP -= 105000;
+  } else if (userXP > 106999){
+
+    //To-Do
+    userLevel = 100;
+    userXP -= 106999;
+
+  }
   
 }
